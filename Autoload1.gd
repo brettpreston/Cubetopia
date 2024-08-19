a2af7f0d41b1b075da38603bced37a6d6698ba22
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-08-19 14:10:59 -0400

    Walkway tilt
    fix
    
    and autoload cleanup

--- Autoload1.gd
+++ Autoload1.gd
@@ -5,9 +5,3 @@ class_name player_data
 static var mapEconomy = 0
 static var mapGlobality = 0
 static var mapTech = 0
-
-# Initialize the variables (optional)
-#func _ready():
-#	cubemapx = 0
-#	cubemapy = 0
-#	cubemapz = 0
