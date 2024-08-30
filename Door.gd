c34014fee9216a7c11cd55c3cca016cae38c5830
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-08-30 17:22:25 -0400

    Door

---
+++ Door.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+# Reference to the AnimationPlayer
+@onready var animation_player = $AnimationPlayer
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+#	if "Player" in body.name:
+		animation_player.play("Close")
--- Gate_40Final.gd
+++ Gate_40Final.gd
@@ -1,11 +1,8 @@
-extends Node
+extends Area3D
 
+var next_scene=load("res://Scenes/Levels/40_Final.tscn")
+var autoload1 = load("res://Autoload1.gd")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body):
+	if "Player" in body.name:
+		get_tree().change_scene_to_packed(next_scene)
