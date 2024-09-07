1c860424ddfb1347450a796b402ae24c82fb8fe8
treepumpkin <davide.distaso@icloud.com>
2024-09-07 11:16:09 +0200

    Fixes to the main menu

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,6 +4,11 @@ var next_scene=load("res://Scenes/Levels/Level0.tscn")
 
 func _ready():
 	music_player.play_bird()
+	%Start.grab_focus()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(next_scene)
+
+
+func _on_quit_pressed():
+	get_tree().quit()
