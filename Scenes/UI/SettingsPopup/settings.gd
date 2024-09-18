717859e716f8c032977ece0398a582a378e2f50f
treepumpkin <davide.distaso@icloud.com>
2024-09-18 20:41:08 +0200

    Added license notice for 3D settings

--- Scenes/UI/SettingsPopup/settings.gd
+++ Scenes/UI/SettingsPopup/settings.gd
@@ -1,3 +1,25 @@
+#Copyright (c) 2014-present Godot Engine contributors. \
+#Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.
+#
+#Permission is hereby granted, free of charge, to any person obtaining
+#a copy of this software and associated documentation files (the
+#"Software"), to deal in the Software without restriction, including
+#without limitation the rights to use, copy, modify, merge, publish,
+#distribute, sublicense, and/or sell copies of the Software, and to
+#permit persons to whom the Software is furnished to do so, subject to
+#the following conditions:
+#
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 extends Control
 
 
@@ -22,7 +44,6 @@ var viewport_start_size := Vector2(
 func _ready() -> void:
 	get_viewport().size_changed.connect(self.update_resolution_label)
 	update_resolution_label()
-
 	# Disable V-Sync to uncap framerate on supported platforms. This makes performance comparison
 	# easier on high-end machines that easily reach the monitor's refresh rate.
 	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
@@ -144,7 +165,7 @@ func _on_fullscreen_option_button_item_selected(index: int) -> void:
 		get_tree().root.set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
 
 
-func _on_fov_slider_value_changed(value: float) -> void:
+func _on_fov_slider_value_changed(_value: float) -> void:
 	#camera.fov = value
 	pass
 
