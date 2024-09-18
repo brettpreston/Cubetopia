414fcfcb3992455e05ff82bd68d8ec946c9154f1
treepumpkin <davide.distaso@icloud.com>
2024-09-18 15:22:17 +0200

    3D settings in the main menu

---
+++ Scenes/UI/SettingsPopup/settings.gd
@@ -0,0 +1,231 @@
+extends Control
+
+
+# Window project settings:
+#  - Stretch mode is set to `canvas_items` (`2d` in Godot 3.x)
+#  - Stretch aspect is set to `expand`
+
+@onready var fps_label := $FPSLabel
+@onready var resolution_label := $ResolutionLabel
+
+var counter := 0.0
+
+# When the screen changes size, we need to update the 3D
+# viewport quality setting. If we don't do this, the viewport will take
+# the size from the main viewport.
+var viewport_start_size := Vector2(
+	ProjectSettings.get_setting(&"display/window/size/viewport_width"),
+	ProjectSettings.get_setting(&"display/window/size/viewport_height")
+)
+
+
+func _ready() -> void:
+	get_viewport().size_changed.connect(self.update_resolution_label)
+	update_resolution_label()
+
+	# Disable V-Sync to uncap framerate on supported platforms. This makes performance comparison
+	# easier on high-end machines that easily reach the monitor's refresh rate.
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+
+func _process(delta: float) -> void:
+	counter += delta
+	# Hide FPS label until it's initially updated by the engine (this can take up to 1 second).
+	fps_label.visible = counter >= 1.0
+	fps_label.text = "%d FPS (%.2f mspf)" % [Engine.get_frames_per_second(), 1000.0 / Engine.get_frames_per_second()]
+	# Color FPS counter depending on framerate.
+	# The Gradient resource is stored as metadata within the FPSLabel node (accessible in the inspector).
+	fps_label.modulate = fps_label.get_meta("gradient").sample(remap(Engine.get_frames_per_second(), 0, 180, 0.0, 1.0))
+
+
+func update_resolution_label() -> void:
+	var viewport_render_size = get_viewport().size * get_viewport().scaling_3d_scale
+	resolution_label.text = "3D viewport resolution: %d × %d (%d%%)" \
+			% [viewport_render_size.x, viewport_render_size.y, round(get_viewport().scaling_3d_scale * 100)]
+
+# Video settings.
+
+func _on_ui_scale_option_button_item_selected(index: int) -> void:
+	var new_scale: float
+	match index:
+		0:
+			new_scale = 0.5
+		1:
+			new_scale = 0.75
+		2:
+			new_scale = 1.0
+		3:
+			new_scale = 1.25
+		4:
+			new_scale = 1.5
+	get_tree().root.content_scale_factor = new_scale
+
+func _on_quality_slider_value_changed(value: float) -> void:
+	get_viewport().scaling_3d_scale = value
+	update_resolution_label()
+
+
+func _on_filter_option_button_item_selected(index: int) -> void:
+	# Viewport scale mode setting.
+	if index == 0: # Bilinear (Fastest)
+		get_viewport().scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
+		# FSR Sharpness is only effective when the scaling mode is FSR 1.0.
+		%FSRSharpnessLabel.visible = false
+		%FSRSharpnessSlider.visible = false
+	elif index == 1: # FSR 1.0 (Fast)
+		get_viewport().scaling_3d_mode = Viewport.SCALING_3D_MODE_FSR
+		# FSR Sharpness is only effective when the scaling mode is FSR 1.0.
+		%FSRSharpnessLabel.visible = true
+		%FSRSharpnessSlider.visible = true
+
+
+func _on_fsr_sharpness_slider_value_changed(value: float) -> void:
+	# Lower FSR sharpness values result in a sharper image.
+	# Invert the slider so that higher values result in a sharper image,
+	# which is generally expected from users.
+	get_viewport().fsr_sharpness = 2.0 - value
+
+
+func _on_vsync_option_button_item_selected(index: int) -> void:
+	# Vsync is enabled by default.
+	# Vertical synchronization locks framerate and makes screen tearing not visible at the cost of
+	# higher input latency and stuttering when the framerate target is not met.
+	# Adaptive V-Sync automatically disables V-Sync when the framerate target is not met, and enables
+	# V-Sync otherwise. This prevents suttering and reduces input latency when the framerate target
+	# is not met, at the cost of visible tearing.
+	if index == 0: # Disabled (default)
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	elif index == 1: # Adaptive
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE)
+	elif index == 2: # Enabled
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+
+
+func _on_limit_fps_slider_value_changed(value: float):
+	# The maximum number of frames per second that can be rendered.
+	# A value of 0 means "no limit".
+	Engine.max_fps = value
+
+
+func _on_msaa_option_button_item_selected(index: int) -> void:
+	# Multi-sample anti-aliasing. High quality, but slow. It also does not smooth out the edges of
+	# transparent (alpha scissor) textures.
+	if index == 0: # Disabled (default)
+		get_viewport().msaa_3d = Viewport.MSAA_DISABLED
+	elif index == 1: # 2×
+		get_viewport().msaa_3d = Viewport.MSAA_2X
+	elif index == 2: # 4×
+		get_viewport().msaa_3d = Viewport.MSAA_4X
+	elif index == 3: # 8×
+		get_viewport().msaa_3d = Viewport.MSAA_8X
+
+
+func _on_taa_option_button_item_selected(index: int) -> void:
+	# Temporal antialiasing. Smooths out everything including specular aliasing, but can introduce
+	# ghosting artifacts and blurring in motion. Moderate performance cost.
+	get_viewport().use_taa = index == 1
+
+
+func _on_fxaa_option_button_item_selected(index: int) -> void:
+	# Fast approximate anti-aliasing. Much faster than MSAA (and works on alpha scissor edges),
+	# but blurs the whole scene rendering slightly.
+	get_viewport().screen_space_aa = int(index == 1) as Viewport.ScreenSpaceAA
+
+
+func _on_fullscreen_option_button_item_selected(index: int) -> void:
+	# To change between winow, fullscreen and other window modes,
+	# set the root mode to one of the options of Window.MODE_*.
+	# Other modes are maximized and minimized.
+	if index == 0: # Disabled (default)
+		get_tree().root.set_mode(Window.MODE_WINDOWED)
+	elif index == 1: # Fullscreen
+		get_tree().root.set_mode(Window.MODE_FULLSCREEN)
+	elif index == 2: # Exclusive Fullscreen
+		get_tree().root.set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
+
+
+func _on_fov_slider_value_changed(value: float) -> void:
+	#camera.fov = value
+	pass
+
+
+func _on_shadow_filter_option_button_item_selected(index):
+	if index == 0: # Very Low
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_HARD)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_HARD)
+	if index == 1: # Low
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_VERY_LOW)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_VERY_LOW)
+	if index == 2: # Medium (default)
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_LOW)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_LOW)
+	if index == 3: # High
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_MEDIUM)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_MEDIUM)
+	if index == 4: # Very High
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_HIGH)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_HIGH)
+	if index == 5: # Ultra
+		RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
+		RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
+
+
+func _on_mesh_lod_option_button_item_selected(index):
+	if index == 0: # Very Low
+		get_viewport().mesh_lod_threshold = 8.0
+	if index == 0: # Low
+		get_viewport().mesh_lod_threshold = 4.0
+	if index == 1: # Medium
+		get_viewport().mesh_lod_threshold = 2.0
+	if index == 2: # High (default)
+		get_viewport().mesh_lod_threshold = 1.0
+	if index == 3: # Ultra
+		# Always use highest LODs to avoid any form of pop-in.
+		get_viewport().mesh_lod_threshold = 0.0
+
+
+# Quality presets.
+
+func _on_very_low_preset_pressed() -> void:
+	%TAAOptionButton.selected = 0
+	%MSAAOptionButton.selected = 0
+	%FXAAOptionButton.selected = 0
+	update_preset()
+
+func _on_low_preset_pressed() -> void:
+	%TAAOptionButton.selected = 0
+	%MSAAOptionButton.selected = 0
+	%FXAAOptionButton.selected = 1
+	update_preset()
+
+
+func _on_medium_preset_pressed() -> void:
+	%TAAOptionButton.selected = 1
+	%MSAAOptionButton.selected = 0
+	%FXAAOptionButton.selected = 0
+	update_preset()
+
+
+func _on_high_preset_pressed() -> void:
+	%TAAOptionButton.selected = 1
+	%MSAAOptionButton.selected = 0
+	%FXAAOptionButton.selected = 0
+	update_preset()
+
+
+func _on_ultra_preset_pressed() -> void:
+	%TAAOptionButton.selected = 1
+	%MSAAOptionButton.selected = 1
+	%FXAAOptionButton.selected = 0
+	update_preset()
+
+
+func update_preset() -> void:
+	# Simulate options being manually selected to run their respective update code.
+	%TAAOptionButton.item_selected.emit(%TAAOptionButton.selected)
+	%MSAAOptionButton.item_selected.emit(%MSAAOptionButton.selected)
+	%FXAAOptionButton.item_selected.emit(%FXAAOptionButton.selected)
+
+
+func _on_hide_button_pressed():
+	hide()
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,14 +1,18 @@
 extends Control
 
-var next_scene=load("res://Scenes/Levels/Level0.tscn")
+const NEXT_SCENE=preload("res://Scenes/Levels/Level0.tscn")
 
 func _ready():
 	music_player.play_bird()
 	%Start.grab_focus()
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_packed(NEXT_SCENE)
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	%Settings.show()
