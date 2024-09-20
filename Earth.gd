d30e0b1ee7f05b41cf9b66f908ec1ca0e51a689b
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-09-19 20:35:05 -0400

    Revert "Removed some warnings"
    This reverts commit 9c7327db2ac5f58651331f8387b0e59a38ee6a73.

--- Earth.gd
+++ Earth.gd
@@ -4,6 +4,6 @@ const ROT_speed = 0.01 #deg rotation per frame
 func _ready():
 	pass
 	
-func _process(_delta):
+func _process(delta):
 	rotate_y(deg_to_rad(ROT_speed))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
