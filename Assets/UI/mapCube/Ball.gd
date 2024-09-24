586e1078929492abaef97218ff5925e4703623b4
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-09-24 17:33:16 -0400

    Map updoot
    it's a subview, the ball moves slowly now..

--- Assets/UI/mapCube/Ball.gd
+++ Assets/UI/mapCube/Ball.gd
@@ -3,6 +3,6 @@ extends MeshInstance3D
 
 # Update sphere position based on autoload variables
 func _process(delta: float) -> void:
-	transform.origin.x = player_data.mapEconomy
-	transform.origin.z = -player_data.mapGlobality
-	transform.origin.y = player_data.mapTech
+	transform.origin.x = lerp(transform.origin.x, float(player_data.mapEconomy), 0.01)
+	transform.origin.z = lerp(transform.origin.z, float(-player_data.mapGlobality), 0.01)
+	transform.origin.y = lerp(transform.origin.y, float(player_data.mapTech), 0.01)
