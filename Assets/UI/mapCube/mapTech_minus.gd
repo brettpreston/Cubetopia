6f6ed96847b573d2bed794e60342baae2d120d1d
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-09-27 18:24:43 -0400

    Trigger code cleanup
    and text outline...

--- Assets/UI/mapCube/mapTech_minus.gd
+++ Assets/UI/mapCube/mapTech_minus.gd
@@ -1,13 +1,6 @@
 extends Area3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+func _on_body_entered(body: Node3D) -> void:
 	if "Player" in body.name:
 		#player_data.mapGlobality += 0
 		player_data.mapTech += -1
