6349b746f5b9ce6c4c856be3c9467c6d819159e1
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-10-01 17:58:06 -0400

    audio trigger

--- audiotrigger.gd
+++ audiotrigger.gd
@@ -1,5 +1,9 @@
 extends Area3D
+@onready var audio_player = $AudioStreamPlayer3D
+@export var audio_stream: AudioStream
+
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		$AudioStreamPlayer3D.stream = audio_stream
 		$AudioStreamPlayer3D.play()
