69f3cbf72f73492edc7de9f69ed0700f92b16d06
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-09-30 22:49:40 -0400

    levelfix

--- Scenes/Levels/Title.gd
+++ Scenes/Levels/Title.gd
@@ -1,22 +1,2 @@
-
-
-
-extends Area3D
+#extends Area3D
 #@export var audio_stream: AudioStreamPlayer3D
-
-func _on_body_entered(body):
-	pass
-	# Attach the audio_player to your scene or relevant node.
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	passww
-
-
-#func _on_body_entered(body):
-#	pass # Replace with function body.
