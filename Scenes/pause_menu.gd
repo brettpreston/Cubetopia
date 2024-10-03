4e498f5ffa324e378d1b4136150c10ca7d6688d6
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-10-03 16:33:45 -0400

    pause

---
+++ Scenes/pause_menu.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+@onready var animator: AnimationPlayer = $AnimationPlayer
+@onready var resume_button: Button = $MarginContainer/VBoxContainer/Resume
+
+func _ready() -> void:
+	resume_button.pressed.connect(unpause)
+
+func unpause():
+	animator.play("Unpause")
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func pause():
+	animator.play("Pause")
+	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_quit_pressed():
+	get_tree().quit()
