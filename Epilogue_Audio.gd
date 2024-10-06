fd0017c6dd641df86a3193ee10ce2deee0b09e8a
evainfeld <ewa.infeld@gmail.com>
2024-10-06 14:14:58 -0400

    text formatting

--- Epilogue_Audio.gd
+++ Epilogue_Audio.gd
@@ -31,14 +31,14 @@ var smallcom: String="You walk among your neighbors and look up at the starry sk
 var global: String="You are on your way to see your friends in the Capital, and look at the world outside the train window. "
 var nature: String="The wind sways the young trees gently, and you can hear the buzzing of bees."
 var space: String="The lights are reflected in the station dome, beyond it in the sky you can see the waxing Earth. "
-var capitalismState: String="You know that some of your friends are struggling, the inequality has not been contained. But the elite have a strong hold on power. Many will be afraid to strive for change."
-var capitalismNoState: String="You know that some of your friends are struggling, the inequality has not been contained. Here and there, you are starting to hear about violent incidents. Perhaps things will have to change again."
-var commerceNature: String="You've made some pastries for the market, and you're excited to see what your neighbors will bring."
-var commerceTech: String="You're excited to show off the new gadgets you made in your workshop at the station's market."
-var central: String="Without existential insecurity, humanity is free to focus on seeking fulfilment and joy."
-var captured: String="Greed for wealth no longer drives us, but greed for power has yet to be stamped out. you ready to fight for the future."
-var volatile: String="You know that complete freedom will require more effort, but you are ready for it. And you know others are too."
-var solidarity: String="There are still some hardships, but the people have learned to work together in a crisis. Humanity is happy."
+var capitalismState: String="You know that some of your friends are struggling, the inequality has not been contained. But the elite have a strong hold on power. Many will be afraid to strive for change. "
+var capitalismNoState: String="You know that some of your friends are struggling, the inequality has not been contained. Here and there, you are starting to hear about violent incidents. Perhaps things will have to change again. "
+var commerceNature: String="You've made some pastries for the market, and you're excited to see what your neighbors will bring. "
+var commerceTech: String="You're excited to show off the new gadgets you made in your workshop at the station's market. "
+var central: String="Without existential insecurity, humanity is free to focus on seeking fulfilment and joy. "
+var captured: String="Greed for wealth no longer drives us, but greed for power has yet to be stamped out. you ready to fight for the future. "
+var volatile: String="You know that complete freedom will require more effort, but you are ready for it. And you know others are too. "
+var solidarity: String="There are still some hardships, but the people have learned to work together in a crisis. Humanity is happy. "
 var utopia: String="The world we fought for is here."
 
 func _ready():
@@ -115,12 +115,12 @@ func play_audio_based_on_choice():
 			audio_queue.append(commerceTechA)
 	else:
 		audio_queue.append(centralA)
-	if player_data.vautonomy < 0:
+	if player_data.vautonomy < 0 and player_data.mapEconomy>-3:
 		if player_data.vstate > 0:
 			audio_queue.append(capturedA)
 		else:
 			audio_queue.append(volatileA)
-	else:
+	elif  player_data.mapEconomy>-3:
 		if player_data.vresilience < 0:
 			audio_queue.append(solidarity)
 		else:
