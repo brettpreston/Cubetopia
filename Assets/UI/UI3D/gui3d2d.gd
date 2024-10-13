1023ca15b871fee5562087a7f55700bc4a071f50
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-10-13 15:54:53 -0400

    Loud quotes

--- Assets/UI/UI3D/gui3d2d.gd
+++ Assets/UI/UI3D/gui3d2d.gd
@@ -1,11 +1,7 @@
 extends Control
-
 var NEXT_SCENE=load("res://Scenes/Levels/00Intro.tscn")
 var Credits=load("res://Scenes/credits.tscn")
 
-#func _ready():
-#	music_player.play_bird()
-#	%Start.grab_focus()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(NEXT_SCENE)
