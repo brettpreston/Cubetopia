ab38f16f0e92671d81a6d743f2461ffd4bf341ae
brettpreston <46427977+brettpreston@users.noreply.github.com>
2024-10-23 01:04:03 -0400

    Step sound fix

--- Scenes/Gates/Gate0.gd
+++ Scenes/Gates/Gate0.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-var next_scene=load("res://Scenes/Levels/Level0.tscn")
+var next_scene=load("res://Scenes/Levels/30Nature.tscn")
 
 func _on_body_entered(body):
 	if "Player" in body.name:
